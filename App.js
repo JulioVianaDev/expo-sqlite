5a2f01a47b10fc8f637a16c7440fc60c8e0c97c4
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-25 14:10:12 -0300

    loading

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as SQLite from 'expo-sqlite';
-
+import { useState } from 'react';
 export default function App() {
-  
+  const db = SQLite.openDatabase('example.db');
+  const [isLoading,setIsLoading] = useState(true);
+  const [names,setNames] = useState([]);
+  const [currentName,setCurrentName] = useState(undefined);
 
+  if (isLoading){
+    return(
+      <View style={styles.container}>
+        <Text>Carregando nomes!</Text>
+      </View>
+    )
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
